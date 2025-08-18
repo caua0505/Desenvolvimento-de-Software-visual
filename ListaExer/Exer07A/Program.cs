b4248cd9c60000b62a3ddce807e9ebdb79a6d4fc
CAUÃ TOBIAS DE SOUZA PROENÇA <caua.tobias012@gmail.com>
2025-08-18 16:56:03 -0300

    first commit

---
+++ ListaExer/Exer07A/Program.cs
@@ -0,0 +1,22 @@
+﻿//Passos para resolver o bubble sort
+//1- Criar array para receber 100 posições 
+//2- Precisamos de um laço de repetição
+// O vetor
+//3- Preencher cada posições com um valor 
+//Aleatorio
+//4-Imprimir o vetor com valores aleatórios 
+
+int[] vetor = new int[100];
+
+Random random = new Random();
+random.Next(100, 1000);
+for (int i = 0; i < 100; i++)
+{
+    //Random 
+    vetor[i] = random.Next(1000);
+}
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(vetor[i]);
+}
